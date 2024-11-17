72a0adab432cfefc549dd5bbf29637b6b085b161
Mohammad Abbad <mohammad.abbad-external@alfuttaim.com>
2024-11-17 15:16:36 +0500

    UI changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from me
+          Learn React from Abbad
         </a>
       </header>
     </div>
